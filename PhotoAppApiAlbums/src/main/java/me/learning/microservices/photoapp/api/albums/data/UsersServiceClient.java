4d5b3789d276fe9dba0b84d7da9879573b621b94
Javier Porto <javirux@gmail.com>
2021-02-14 18:06:27 +0100

    Put service name in a static constant field

--- PhotoAppApiAlbums/src/main/java/me/learning/microservices/photoapp/api/albums/data/UsersServiceClient.java
+++ PhotoAppApiAlbums/src/main/java/me/learning/microservices/photoapp/api/albums/data/UsersServiceClient.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 import me.learning.microservices.photoapp.api.albums.ui.model.UserResponse;
 
-@FeignClient(name = "users-ws", fallbackFactory = UsersServiceFallbackFactory.class)
+@FeignClient(name = UsersServiceClient.USERS_SERVICE_NAME, fallbackFactory = UsersServiceFallbackFactory.class)
 public interface UsersServiceClient {
+
+    String USERS_SERVICE_NAME = "users-ws";
 
     @GetMapping(path = "/users/{userId}")
     UserResponse getUser(@RequestHeader("Authorization") String authToken, @PathVariable String userId);
