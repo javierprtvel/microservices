ad4942d40d973297825e125d4cbd9d7d0bb3194e
Javier Porto <javierprtvel@gmail.com>
2021-02-15 21:24:20 +0100

    #1 Style improvement

--- PhotoAppApiAlbums/src/main/java/me/learning/microservices/photoapp/api/albums/service/exception/AlbumNotFoundException.java
+++ PhotoAppApiAlbums/src/main/java/me/learning/microservices/photoapp/api/albums/service/exception/AlbumNotFoundException.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Album not found")
 public class AlbumNotFoundException extends Exception {
 
-    public AlbumNotFoundException() {
-    }
+    public AlbumNotFoundException() {}
 
     public AlbumNotFoundException(String message) {
         super(message);
